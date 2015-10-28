092e1ba1d34c0c00333df34b9deb51d137e835f1
Stephen Paradis <sparadis@localhost.localdomain>
2015-10-28 11:56:58 -0400

    update to union

---
+++ union.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+union card{
+	int rank;
+	int suit;
+};
+
+int main(void){
+	union card ace_of_clubs;
+	ace_of_clubs.rank = 514;
+	ace_of_clubs.suit = 'C';
+
+	printf("%d of %c\n", ace_of_clubs.rank, ace_of_clubs.suit);
+}
