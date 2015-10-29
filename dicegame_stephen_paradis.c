bd5457dbf6d1018e3967aac6552abd8ee786b94d
Stephen Paradis <sparadis@localhost.localdomain>
2015-10-29 08:10:20 -0400

    Latest update to command line input

--- dicegame_stephen_paradis.c
+++ dicegame_stephen_paradis.c
@@ -14,15 +14,15 @@ int main (int argc, char *argv[])
 	int total[13];
 	int frequency[13] = { 0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1 }; //array for probability of 2 dice outcomes
 	int input;
-	if(argc == 1)
+	if(argc == 1) //checks if there is only one argument in the command line
 	{
 		printf("No values given, value defaulted to 100\n");
 		diceroll = 100;
 		input = 1;
 	}
-	else if(argc >= 2)
+	else if(argc == 2) //checks if there is more then the filename as an argument
 	{
-		if (isalpha(*argv[1]))
+		if (!isdigit(*argv[1]))
 		{
 			printf("Command line argument [1] not a number; Defaulted to 99\n");
 			diceroll = 100;
@@ -31,24 +31,45 @@ int main (int argc, char *argv[])
 		else if (isdigit(*argv[1]))
 		{
 			diceroll = atoi(argv[1]);
-			printf("Please enter histogram integer: ");
-			scanf("%d", &input);
+			input = 1;
 		}
+	}
+	else if(argc > 2)
+	{
+
+		if (!isdigit(*argv[2]))
+		{
+			printf("Histogram defaulted to 1\n");
+			input = 1;
+			diceroll = atoi(argv[1]);
+		}
+		else if(isdigit(*argv[2]))
+		{
+			input = atoi(argv[2]);
+			diceroll = atoi(argv[1]);
+			if (input <= 0)
+			{
+				printf("That was invalid, histogram defaulted to 1\n");
+				input = 1;
+			}
+		}
+
+	
 	}
 	printf("The histogram number is %d\n", input);
 	for(numbercounter = 2; numbercounter <= 12; numbercounter++)
 	{
 		total[numbercounter] = 0;	//initializes each value count from 2 to 12 to 0;
 	}
-
+	//loop to randomly roll dice and keep count of each value obtained
 	for(numbercounter = 0; numbercounter < diceroll; numbercounter++)
 	{
-		double dice1 = (rand() % 6) + 1; //1 thru 6
-		double dice2 = (rand() % 6) + 1; //1 thru 6
+		double dice1 = (rand() % 6) + 1; //random number 1 thru 6
+		double dice2 = (rand() % 6) + 1; //random number 1 thru 6
 		totalcount = dice1 + dice2;
-		total[totalcount]++; //increases the total of the sum of the dice by one each turn
+		total[totalcount]++;
 	}
-
+	//prints out number value of dice, number of times rolled, and percentage the number of times rolled
 	printf("Number of times rolled %d\n", diceroll);
 	for(numbercounter = 2; numbercounter < 13; numbercounter++)
 	{
@@ -57,6 +78,7 @@ int main (int argc, char *argv[])
 	}
 
 	printf("Expected information:\n");
+	//prints out number value and expected rools, and percentage for each expected value
 	for(numbercounter = 2; numbercounter < 13; numbercounter++)
 	{
 		double expected_percent = (100.0 * frequency[numbercounter] / 36);
@@ -66,18 +88,20 @@ int main (int argc, char *argv[])
 	}
 
 	printf("Histogram information: \n");
+	//prints out number value and "*'s" based on given histogram number
 	for(numbercounter = 2; numbercounter < 13; numbercounter++)
 	{
 		double answer = (total[numbercounter]/input);
 		int remainder = (total[numbercounter] % input);
 		printf("Number: %d\t", numbercounter);
-		histogram(answer, remainder);
+		histogram(answer, remainder); //calls histogram values needed to print out "*'s"
 	}
 	return(0);
 }
 void histogram(double answer, int remainder)
 {
 	int x;
+	//prints out "*'s" based on number input in
 	for (x = 1; x <= answer; ++x)
 	{
 		printf("*");
