9e9e66c39ba94bf39f9bae9117f9610bebd84312
Stephen Paradis <sparadis@localhost.localdomain>
2015-10-29 14:46:42 -0400

    Moved into functions

--- dicegame_stephen_paradis.c
+++ dicegame_stephen_paradis.c
@@ -2,14 +2,14 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <time.h>
-
-void histogram(double answer, int remainder);
+//prototypes
+void histogram_print(int total, int input, int numbercounter);
+void expected_print(int numbercounter, int diceroll, double math1, int math2);
 
 int main (int argc, char *argv[])
 {
 	srand(time(NULL));
 	int diceroll;
-	int numbercounter;
 	int totalcount;
 	int total[13];
 	int frequency[13] = { 0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1 }; //array for probability of 2 dice outcomes
@@ -24,7 +24,7 @@ int main (int argc, char *argv[])
 	{
 		if (!isdigit(*argv[1]))
 		{
-			printf("Command line argument [1] not a number; Defaulted to 99\n");
+			printf("Command line argument [1] not a number; Defaulted to 100\n");
 			diceroll = 100;
 			input = 1;
 		}
@@ -42,6 +42,12 @@ int main (int argc, char *argv[])
 			printf("Histogram defaulted to 1\n");
 			input = 1;
 			diceroll = atoi(argv[1]);
+			if (diceroll <= 0)
+			{
+				printf("Command line argument [1] less then 0; Defaulted to 100\n");
+				diceroll = 100;
+			}
+			
 		}
 		else if(isdigit(*argv[2]))
 		{
@@ -52,56 +58,51 @@ int main (int argc, char *argv[])
 				printf("That was invalid, histogram defaulted to 1\n");
 				input = 1;
 			}
+			if (diceroll <= 0)
+			{
+				printf("Command line argument [1] less then 0; Defaulted to 100\n");
+				diceroll = 100;
+			}
 		}
 
 	
 	}
 	printf("The histogram number is %d\n", input);
-	for(numbercounter = 2; numbercounter <= 12; numbercounter++)
+	for(int numbercounter = 2; numbercounter <= 12; numbercounter++)
 	{
 		total[numbercounter] = 0;	//initializes each value count from 2 to 12 to 0;
 	}
 	//loop to randomly roll dice and keep count of each value obtained
-	for(numbercounter = 0; numbercounter < diceroll; numbercounter++)
+	for(int numbercounter = 0; numbercounter < diceroll; numbercounter++)
 	{
 		double dice1 = (rand() % 6) + 1; //random number 1 thru 6
 		double dice2 = (rand() % 6) + 1; //random number 1 thru 6
 		totalcount = dice1 + dice2;
 		total[totalcount]++;
 	}
-	//prints out number value of dice, number of times rolled, and percentage the number of times rolled
 	printf("Number of times rolled %d\n", diceroll);
-	for(numbercounter = 2; numbercounter < 13; numbercounter++)
-	{
-		double percentage = (100.0 * total[numbercounter] / diceroll);
-		printf("Number: %d\tNumber of times rolled: %d\tPercentage: %.2f%%\n",numbercounter,total[numbercounter], percentage);
-	}
-
 	printf("Expected information:\n");
-	//prints out number value and expected rools, and percentage for each expected value
-	for(numbercounter = 2; numbercounter < 13; numbercounter++)
+	for(int numbercounter = 2; numbercounter < 13; numbercounter++)
 	{
-		double expected_percent = (100.0 * frequency[numbercounter] / 36);
-		int expected = (diceroll * frequency[numbercounter] / 36);
-		double percentage = (100.0 * total[numbercounter] / diceroll);
-		printf("Number: %d\tExpected rolls: %d\tPercentage of expected: %.2f%%\t Difference: %.2f%%\n", numbercounter, expected, expected_percent, (percentage - expected_percent));
+		//calls expected print function
+		expected_print(numbercounter, diceroll, frequency[numbercounter], total[numbercounter]);
 	}
 
 	printf("Histogram information: \n");
-	//prints out number value and "*'s" based on given histogram number
-	for(numbercounter = 2; numbercounter < 13; numbercounter++)
-	{
-		double answer = (total[numbercounter]/input);
-		int remainder = (total[numbercounter] % input);
-		printf("Number: %d\t", numbercounter);
-		histogram(answer, remainder); //calls histogram values needed to print out "*'s"
+	for(int numbercounter = 2; numbercounter < 13; numbercounter++)
+	{	
+		//calls histogram function to print out '*s'	
+		histogram_print(total[numbercounter], input, numbercounter);
 	}
 	return(0);
 }
-void histogram(double answer, int remainder)
+//prints out '*s' based on number input in
+void histogram_print(int total, int input, int numbercounter)
 {
 	int x;
-	//prints out "*'s" based on number input in
+	double answer = (total/input);
+	int remainder = (total % input);
+	printf("Number: %d\t", numbercounter);
 	for (x = 1; x <= answer; ++x)
 	{
 		printf("*");
@@ -109,3 +110,12 @@ void histogram(double answer, int remainder)
 	printf("%s: %d","    Remainder", remainder);
 	printf("\n");
 }
+//prints out number value and expected rools, and percentage for rolled and expected and difference betweent the 2
+void expected_print(int numbercounter, int diceroll, double frequency, int total)
+{
+	double expected_percent = (100.0 * frequency / 36);
+	double rolled_percent = (100.0 * total / diceroll);
+	double difference = (rolled_percent - expected_percent);
+	printf("Number: %d\tPercent of rolled %.2f%%\t\tPercent of expected: %.2f%%\t Difference: %.2f%%\n", numbercounter, 	rolled_percent, expected_percent, difference);
+}
+
